f7d3781fa4ed7e07122ff4296d7bb07ca180ac13
Emmanuel Velez <emmanuelvelezvalencia@gmail.com>
2026-04-13 17:08:53 -0500

    se corrigen errores en las clases Guest y Reservation

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -6,33 +6,31 @@ from app.services.util import (generate_unique_id, date_lower_than_today_error,
     reservation_not_found_error, guest_not_found_error, room_not_available_error,
     room_not_found_error, room_already_exists_error)
 
-
+@dataclass
 class Guest:
-    VIP:str = 'vip'
-    REGULAR:str = 'regular'
+    VIP = 'vip'
+    REGULAR = 'regular'
 
-    def __init__(self, name: str, email: str, type_: REGULAR):
-        self.name = name
-        self.email = email
-        self.type_ = type_
+    name: str
+    email: str
+    type_: str = REGULAR
+
     def __str__(self) -> str:
         return f"Guest {self.name} ({self.email}) of type {self.type_}"
 
-dataclass(Guest)
 @dataclass
 class Reservation:
 
-    def __init__(self, guest_name: str, description: str, check_in: date, check_out: date):
-        self.guest_name = guest_name
-        self.guest_email = guest_name
-        self.description = description
-        self.check_in = check_in
-        self.check_out = check_out
-        self.guests: list[Guest] = field(default_factory=list)
-        self.id: str = field(default_factory=generate_unique_id)
+    guest_name: str
+    guest_email: str
+    description: str
+    check_in: date
+    check_out: date
+    guests: list[Guest] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
 
     def add_guest(self, name: str, email: str, type_: str = Guest.REGULAR):
-        guest = Guest(name=name, email=email, type_=type_)
+        guest = Guest(name= name, email= email, type_= type_)
         self.guests.append(guest)
 
     def delete_guest(self, guest_index: int):
@@ -42,7 +40,12 @@ class Reservation:
             guest_not_found_error()
 
     def __len__(self) -> int:
-        return len(self.guests)
+        return (self.check_out - self.check_in).days
 
     def __str__(self) -> str:
-        return f"Guest: {self.guest_name}\nDescription: {self.description}\nDates: {self.check_in} - {self.check_out}"
+        return (
+            f"ID: {self.id}\n"
+            f"Guest: {self.guest_name}\n"
+            f"Description: {self.description}\n"
+            f"Dates: {self.check_in} - {self.check_out}"
+        )
