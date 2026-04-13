7f89938011d7eda8a1e2aa94557265f1bacfa1fa
Emmanuel Velez <emmanuelvelezvalencia@gmail.com>
2026-04-13 16:55:45 -0500

    se implementan las clases Guest y Reservation

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -7,13 +7,42 @@ from app.services.util import (generate_unique_id, date_lower_than_today_error,
     room_not_found_error, room_already_exists_error)
 
 
-# TODO: Implement Guest class here
+class Guest:
+    VIP:str = 'vip'
+    REGULAR:str = 'regular'
 
+    def __init__(self, name: str, email: str, type_: REGULAR):
+        self.name = name
+        self.email = email
+        self.type_ = type_
+    def __str__(self) -> str:
+        return f"Guest {self.name} ({self.email}) of type {self.type_}"
 
-# TODO: Implement Reservation class here
+dataclass(Guest)
+@dataclass
+class Reservation:
 
+    def __init__(self, guest_name: str, description: str, check_in: date, check_out: date):
+        self.guest_name = guest_name
+        self.guest_email = guest_name
+        self.description = description
+        self.check_in = check_in
+        self.check_out = check_out
+        self.guests: list[Guest] = field(default_factory=list)
+        self.id: str = field(default_factory=generate_unique_id)
 
-# TODO: Implement Room class here
+    def add_guest(self, name: str, email: str, type_: str = Guest.REGULAR):
+        guest = Guest(name=name, email=email, type_=type_)
+        self.guests.append(guest)
 
+    def delete_guest(self, guest_index: int):
+        if 0 <= guest_index < len(self.guests):
+            del self.guests[guest_index]
+        else:
+            guest_not_found_error()
 
-# TODO: Implement Hotel class here
+    def __len__(self) -> int:
+        return len(self.guests)
+
+    def __str__(self) -> str:
+        return f"Guest: {self.guest_name}\nDescription: {self.description}\nDates: {self.check_in} - {self.check_out}"
